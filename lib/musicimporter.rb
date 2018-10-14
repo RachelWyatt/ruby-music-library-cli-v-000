b539b134cca01eaf5191fde13d55cf0b990fbc3a
Rachel Wyatt <rw126@evansville.edu>
2018-10-14 13:31:53 +0000

    automatically backed up by learn

--- lib/musicimporter.rb
+++ lib/musicimporter.rb
@@ -5,7 +5,7 @@ attr_reader :path
   end
 
   def files
-    files = Dir.glob("#{path}/*").map { |file| file.gsub("#{path}/", '')}
+    Dir.glob("#{path}/*").map { |file| file.gsub("#{path}/", '')}
   end
 
   def import
