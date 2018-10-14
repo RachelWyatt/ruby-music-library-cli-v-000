9983d9524595730a81051f7844e4b4fd19131c7d
Rachel Wyatt <rw126@evansville.edu>
2018-10-14 13:25:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,6 +54,7 @@ class Song
 
   def self.new_from_filename(name)
     artist, song, genre = name.split(" - ")
+    genre.gsub('.mp3', '')
     artist = Artist.find_or_create_by_name(artist)
     genre = Genre.find_or_create_by_name(genre)
     new(song, artist, genre)
