0281f5656a21e0cb4d109767405e431f649daf10
Rachel Wyatt <rw126@evansville.edu>
2018-10-14 13:37:48 +0000

    automatically backed up by learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -4,8 +4,9 @@ class MusicLibraryController
     MusicImporter.new(path).import
   end
 
-  def call 
+  def call
     puts "Welcome to your music library!"
+    puts "To list all of your songs, enter 'list songs'."
 
   end
 
